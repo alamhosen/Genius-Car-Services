be691ffd333dff98d55af737740a6d2c09d1603d
Alam Hosen <alamhosen.web@gmail.com>
2022-04-14 00:00:18 +0600

    login and register page done by using react firebase hooks

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
 import './Register.css'
+import auth from '../../../firebase.init';
 
 const Register = () => {
     const navigate = useNavigate()
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
 
     const navigateLogin = () => {
         navigate('/login')
@@ -14,7 +23,11 @@ const Register = () => {
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(name, email, password);
+        createUserWithEmailAndPassword(email, password);
+    }
+
+    if(user){
+        navigate('/home');
     }
 
     return (
