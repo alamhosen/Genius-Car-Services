b299558be3d37de24cd61e503229988147b24c29
Alam Hosen <alamhosen.web@gmail.com>
2022-04-14 00:36:31 +0600

    RequireAuth applied in checkout page

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const {serviceID} = useParams()
     return (
         <div>
             <h2>Welcome to service detail: {serviceID}</h2>
+            <div className='text-center'>
+                <Link to='/checkout' className='btn btn-primary'>Proceed Checkout</Link>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,17 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Col, Container, Nav, Navbar, NavDropdown, Row } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import logo from '../../../images/logo.png'
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignout = () =>{
+        signOut(auth);
+    }
     return (
         <>
             <Navbar collapseOnSelect sticky='top' expand="lg" bg="primary" variant="dark">
@@ -26,9 +34,12 @@ const Header = () => {
                         </Nav>
                         <Nav>
                             <Nav.Link as={Link} to="about">About</Nav.Link>
-                            <Nav.Link as={Link} to="login">
-                                Login
-                            </Nav.Link>
+                            {user ? 
+                            <button className='btn btn-link text-white text-decoration-none' onClick={handleSignout}>Sign Out</button>
+                            :
+                                <Nav.Link as={Link} to="login">
+                                    Login
+                                </Nav.Link>}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
